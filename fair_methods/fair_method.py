573f63646749bc0295921458346c331b7ab347f7
Beñat Descalzo <bdescalzo001@ikasle.ehu.eus>
2026-02-11 13:05:05 +0100

    Add base model to meta-learning

--- fair_methods/fair_method.py
+++ fair_methods/fair_method.py
@@ -2,7 +2,7 @@ class FairMethod:
     def __init__(self, **kwargs):
         pass
 
-    def load_data(self, dataloader):
+    def load_data(self, dataloader_train, dataloader_test):
         pass
 
     def fit(self, sensitive_labels, **kwargs):
