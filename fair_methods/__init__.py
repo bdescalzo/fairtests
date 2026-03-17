048a0deaf48c45b60a22de3fead95a397f43bc7a
Beñat Descalzo <bdescalzo001@ikasle.ehu.eus>
2026-03-17 03:36:06 +0100

    Add MMPF

--- fair_methods/__init__.py
+++ fair_methods/__init__.py
@@ -1,6 +1,13 @@
 from .fair_method import FairMethod
 from .meta import MetaLearning
 from .baseline import Baseline
+from .mmpf import MinimaxParetoFairness
 from .reptile import Reptile
 
-__all__ = ["FairMethod", "MetaLearning", "Baseline", "Reptile"]
+__all__ = [
+    "FairMethod",
+    "MetaLearning",
+    "Baseline",
+    "MinimaxParetoFairness",
+    "Reptile",
+]
